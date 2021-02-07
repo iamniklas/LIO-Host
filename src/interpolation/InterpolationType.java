2fefe23e2df24d122fc5ae68943bccdbcb10967f
iamniklas <niklas.englmeier@web.de>
2021-02-07 18:59:47 +0100

    Interpolation

---
+++ src/interpolation/InterpolationType.java
@@ -0,0 +1,14 @@
+package interpolation;
+
+public enum InterpolationType {
+	EaseInSine, 	EaseOutSine, 	EaseInOutSine,
+	EaseInQuad, 	EaseOutQuad, 	EaseInOutQuad,
+	EaseInCubic, 	EaseOutCubic, 	EaseInOutCubic,
+	EaseInQuart, 	EaseOutQuart, 	EaseInOutQuart,
+	EaseInQuint, 	EaseOutQuint, 	EaseInOutQuint,
+	EaseInExpo, 	EaseOutExpo, 	EaseInOutExpo,
+	EaseInCirc, 	EaseOutCirc, 	EaseInOutCirc,
+	EaseInBack, 	EaseOutBack, 	EaseInOutBack,
+	EaseInElastic, 	EaseOutElastic, EaseInOutElastic,
+	EaseInBounce, 	EaseOutBounce, 	EaseInOutBounce
+}
