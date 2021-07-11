61aebf1dc7978a3cab2a275662be94638212c6cc
iamniklas <niklas.englmeier@web.de>
2021-07-11 14:35:28 +0200

    Progress Procedure

--- src/procedures/ProgressProcedure.java
+++ src/procedures/ProgressProcedure.java
@@ -1,5 +1,6 @@
 package procedures;
 
+import led.ColorRGB;
 import led.LEDDataBundle;
 import led.LEDStripManager;
 import led.ProcedureBundleFields;
@@ -9,8 +10,15 @@ public class ProgressProcedure extends Procedure {
 
 	public ProgressBarData mProgressData = new ProgressBarData();
 	
+	private float mProgress = 0.0f;
+	private ColorRGB mColor; 
+	
 	public ProgressProcedure(LEDDataBundle _bundle) {
 		super(_bundle);
+		
+		mColor = (ColorRGB)_bundle.get(ProcedureBundleFields.COLOR_PRIMARY);
+		mProgress = (float) ((double)_bundle.get(ProcedureBundleFields.VALUE_1));
+		mSteps = 1;
 	}
 
 	@Override
@@ -20,6 +28,14 @@ public class ProgressProcedure extends Procedure {
 
 	@Override
 	void update() {
+		mStrip.setAllPixels(ColorRGB.black.toSystemColor());
+		mStrip.setArea(0, (int)(LEDStripManager.LED_COUNT * mProgress), mColor.toSystemColor());
+		postUpdate();
 		
+		if (mStep == mSteps) {
+			finishProcedure();
+		}
+		
+		mStep++;
 	}
 }
